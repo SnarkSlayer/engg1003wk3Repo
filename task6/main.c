bcab4fe400831ddc2e9f37d9226d4466bf3fa818
Jakob Flannery <c3355432@uon.edu.au>
2020-03-10 15:16:18 +1100

    Fixed fibonacci stuff

--- task6/main.c
+++ task6/main.c
@@ -2,15 +2,15 @@
 
 int main()
 {
-    float b1 = 1, b2 = 1;
-    float x1 = 1, x2 = 1;
+    float b1 = 0.2, b2 = 0.8;
+    float x1 = 1, x2 = 0.5;
     float tmp;
 
     printf("X1: %f\n", x1);
     for (int i = 0; i < 9; i++) {
         printf("X%d: %f\n", (i + 2), x2);
         tmp = x2;
-        x2 = b1*x1 + b2*x2;
+        x2 = b1*x2 + b2*x1;
         x1 = tmp;
     }
 
--- task7/main.c
+++ task7/main.c
@@ -19,7 +19,7 @@ int main()
     for (int i = 0; i < 19; i++) {
         printf("X%d: %f\n", (i + 2), x2);
         tmp = x2;
-        x2 = b1*x1 + b2*x2;
+        x2 = b1*x2 + b2*x1;
         x1 = tmp;
         //TESTING OSCILLATION
         //testing diff
